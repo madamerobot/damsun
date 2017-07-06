72d3951aaa4374f14d141e58a592d8388e0cb173
Valerie Fuchs <hi@valerie-fuchs.de>
2017-07-06 11:06:08 +0200

    Added bigger JSON file to test filters, working great

--- src/sunspots.js
+++ src/sunspots.js
@@ -1,6 +1,14 @@
 var DATA = 
 
-	[{"Place":"Blijburg","Touristfactor":1,"Price":1,"Weatherproof":1},
-	{"Place":"Vondelpark","Touristfactor":5,"Price":5,"Weatherproof":5},]
+[{"Place":"Blijburg","Touristfactor":3,"Price":1,"Weatherproof":1},
+{"Place":"Vondelpark","Touristfactor":5,"Price":1,"Weatherproof":3},
+{"Place":"Frederiksplein","Touristfactor":3,"Price":1,"Weatherproof":1},
+{"Place":"Haarlemmerplein","Touristfactor":4,"Price":1,"Weatherproof":4},
+{"Place":"Strand West","Touristfactor":3,"Price":1,"Weatherproof":3},
+{"Place":"Pllek","Touristfactor":4,"Price":4,"Weatherproof":4},
+{"Place":"Roest","Touristfactor":3,"Price":3,"Weatherproof":4},
+{"Place":"Strand Zuid","Touristfactor":1,"Price":2,"Weatherproof":3},
+{"Place":"Twiske","Touristfactor":1,"Price":2,"Weatherproof":1},
+{"Place":"De Natureluur","Touristfactor":2,"Price":3,"Weatherproof":1}]
 
 export default DATA;
